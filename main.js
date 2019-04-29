12c073068fa697a1cec28cb5c29906774339cfc8
Muñoz Soto <jcmunozs@mad.azertia.com>
2019-04-29 17:03:50 +0200

    v 0.8.5

--- main.js
+++ main.js
@@ -4,10 +4,10 @@ squares = [], [];
 for (let i = 0; i < 8; i++) {
     squares.push(["", "", "", "", "", "", "", ""]);
 }
-console.log(squares);
 createTable();
 function createTable() {
     let table = document.getElementById("table");
+    table.innerHTML = "";
     for (let i = 0; i < squares.length; i++) {
         let row = document.createElement("div");
         row.classList.add("row");
@@ -74,14 +74,19 @@ function createTable() {
         table.appendChild(row);
     }
 }
-function addToken(parent, type) {
+function addToken(parent, type, queen) {
     squareList = document.getElementsByClassName("square");
     let token = document.createElement("div");
     token.classList.add(type);
     token.classList.add("token");
-
-
+    let crown = document.createElement("img");
+    crown.src = "img/crown.png";
+    crown.classList.add("crown");
     if (type == 'blackToken') {
+        if (!token.classList.contains("queenToken") && parent.getAttribute("x") == 7 || queen) {
+            token.classList.add("queenToken");
+            token.appendChild(crown);
+        }
         token.onclick = (e) => {
             let x = parseInt(token.parentElement.getAttribute("x"));
             let y = parseInt(token.parentElement.getAttribute("y"));
@@ -93,16 +98,62 @@ function addToken(parent, type) {
             for (let i = 0; i < squareList.length; i++) {
                 element = squareList[i];
 
-                if (squareList[i].hasChildNodes() == true &&
+                //BLACK QUEEN EAT 1
+                if (squareList[i].hasChildNodes() &&
+                    parent.childNodes[0].classList.contains("queenToken") &&
+                    squareList[i].childNodes[0].classList.contains("whiteToken") &&
+                    squareList[i].getAttribute("x") == x + 1 &&
+                    squareList[i].getAttribute("y") == y + 1 &&
+                    squareList[i - 7] &&
+                    !squareList[i - 7].hasChildNodes()) {
+                    if (turn)   squareList[i - 7].classList.add("moveOption");
+                    squareList[i - 7].onclick = (e) => {
+                        if (turn) {
+                            parent.innerHTML = "";
+                            squareList[i].innerHTML = "";
+                            addToken(e.target, type, true);
+                            for (let j = 0; j < squareList.length; j++) {
+                                squareList[j].classList.remove("moveOption");
+                                squareList[j].onclick = () => { }
+                            }
+                            turn = !turn;
+
+                        }
+                    }
+                }
+                // BLACK QUEEN EAT 2
+                if (squareList[i].hasChildNodes() &&
+                    parent.childNodes[0].classList.contains("queenToken") &&
+                    squareList[i].childNodes[0].classList.contains("whiteToken") &&
+                    squareList[i].getAttribute("x") == x - 1 &&
+                    squareList[i].getAttribute("y") == y + 1 &&
+                    squareList[i - 9] &&
+                    !squareList[i - 9].hasChildNodes()) {
+                    if (turn)   squareList[i - 9].classList.add("moveOption");
+                    squareList[i - 9].onclick = (e) => {
+                        if (turn) {
+                            parent.innerHTML = "";
+                            squareList[i].innerHTML = "";
+                            addToken(e.target, type, true);
+                            for (let j = 0; j < squareList.length; j++) {
+                                squareList[j].classList.remove("moveOption");
+                                squareList[j].onclick = () => { }
+                            }
+                            turn = !turn;
+                        }
+                    }
+
+                }
+                // BLACK EAT 1
+                if (squareList[i].hasChildNodes() &&
                     squareList[i].childNodes[0].classList.contains("whiteToken") &&
                     squareList[i].getAttribute("x") == x + 1 &&
                     squareList[i].getAttribute("y") == y - 1 &&
                     squareList[i + 7] &&
-                    squareList[i + 7].hasChildNodes() == false) {
-                    squareList[i + 7].classList.add("moveOption");
+                    !squareList[i + 7].hasChildNodes()) {
+                    if (turn)  squareList[i + 7].classList.add("moveOption");
                     squareList[i + 7].onclick = (e) => {
                         if (turn) {
-                            console.log(e.target);
                             parent.innerHTML = "";
                             squareList[i].innerHTML = "";
                             addToken(e.target, type);
@@ -111,22 +162,21 @@ function addToken(parent, type) {
                                 squareList[j].onclick = () => { }
                             }
                             turn = !turn;
+
                         }
                     }
-
                 }
 
-
-                if (squareList[i].hasChildNodes() == true &&
+                //BLACK EAT 2
+                if (squareList[i].hasChildNodes() &&
                     squareList[i].childNodes[0].classList.contains("whiteToken") &&
                     squareList[i].getAttribute("x") == x + 1 &&
                     squareList[i].getAttribute("y") == y + 1 &&
                     squareList[i + 9] &&
-                    squareList[i + 9].hasChildNodes() == false) {
-                    squareList[i + 9].classList.add("moveOption");
+                    !squareList[i + 9].hasChildNodes()) {
+                    if (turn)   squareList[i + 9].classList.add("moveOption");
                     squareList[i + 9].onclick = (e) => {
                         if (turn) {
-                            console.log(e.target);
                             parent.innerHTML = "";
                             squareList[i].innerHTML = "";
                             addToken(e.target, type);
@@ -139,17 +189,16 @@ function addToken(parent, type) {
                     }
 
                 }
-
-                if (element.hasChildNodes() == false &&
+                //BLACK MOVE
+                if (!element.hasChildNodes() &&
                     element.getAttribute("x") == x + 1 &&
                     element.getAttribute("y") == y + 1 ||
-                    element.hasChildNodes() == false &&
+                    !element.hasChildNodes() &&
                     element.getAttribute("x") == x + 1 &&
                     element.getAttribute("y") == y - 1) {
-                    element.classList.add("moveOption");
+                    if (turn) element.classList.add("moveOption");
                     element.onclick = function move(e) {
                         if (turn) {
-                            console.log(e.target);
                             parent.innerHTML = "";
                             addToken(e.target, type);
                             for (let j = 0; j < squareList.length; j++) {
@@ -165,11 +214,41 @@ function addToken(parent, type) {
                         break;
                     }
                 }
+                //BLACK QUEEN MOVE
+                if (!element.hasChildNodes() &&
+                    parent.childNodes[0].classList.contains("queenToken") &&
+                    element.getAttribute("x") == x - 1 &&
+                    element.getAttribute("y") == y - 1 ||
+                    !element.hasChildNodes() &&
+                    parent.childNodes[0].classList.contains("queenToken") &&
+                    element.getAttribute("x") == x - 1 &&
+                    element.getAttribute("y") == y + 1) {
+                    if (turn) element.classList.add("moveOption");
+                    element.onclick = function move(e) {
+                        if (turn) {
+                            parent.innerHTML = "";
+                            addToken(e.target, type, true);
+                            for (let j = 0; j < squareList.length; j++) {
+                                squareList[j].classList.remove("moveOption");
+                                squareList[j].onclick = () => { }
+                            }
+                            turn = !turn;
+                        }
 
+                    }
+                    nsquare++
+                    if (nsquare == 2) {
+                        break;
+                    }
+                }
             }
         }
     }
     else {
+        if (parent.getAttribute("x") == 0) {
+            token.classList.add("queenToken");
+            token.appendChild(crown);
+        }
         token.onclick = (e) => {
             let x = parseInt(token.parentElement.getAttribute("x"));
             let y = parseInt(token.parentElement.getAttribute("y"));
@@ -179,16 +258,16 @@ function addToken(parent, type) {
             }
             for (let i = 0; i < squareList.length; i++) {
                 element = squareList[i];
+                //WHITE EAT 1
                 if (squareList[i].hasChildNodes() == true &&
                     squareList[i].childNodes[0].classList.contains("blackToken") &&
                     squareList[i].getAttribute("x") == x - 1 &&
                     squareList[i].getAttribute("y") == y + 1 &&
                     squareList[i - 7] &&
                     squareList[i - 7].hasChildNodes() == false) {
-                    squareList[i - 7].classList.add("moveOption");
+                    if (!turn) squareList[i - 7].classList.add("moveOption");
                     squareList[i - 7].onclick = (e) => {
                         if (!turn) {
-                            console.log(e.target);
                             parent.innerHTML = "";
                             squareList[i].innerHTML = "";
                             addToken(e.target, type);
@@ -202,17 +281,16 @@ function addToken(parent, type) {
 
                 }
 
-
+                //WHITE EAT 2
                 if (squareList[i].hasChildNodes() == true &&
                     squareList[i].childNodes[0].classList.contains("blackToken") &&
                     squareList[i].getAttribute("x") == x - 1 &&
                     squareList[i].getAttribute("y") == y - 1 &&
                     squareList[i - 9] &&
                     squareList[i - 9].hasChildNodes() == false) {
-                    squareList[i - 9].classList.add("moveOption");
+                    if (!turn) squareList[i - 9].classList.add("moveOption");
                     squareList[i - 9].onclick = (e) => {
                         if (!turn) {
-                            console.log(e.target);
                             parent.innerHTML = "";
                             squareList[i].innerHTML = "";
                             addToken(e.target, type);
@@ -225,16 +303,16 @@ function addToken(parent, type) {
                     }
 
                 }
+                //WHITE MOVE
                 if (squareList[i].hasChildNodes() == false &&
                     squareList[i].getAttribute("x") == x - 1 &&
                     squareList[i].getAttribute("y") == y + 1 ||
                     squareList[i].hasChildNodes() == false &&
                     squareList[i].getAttribute("x") == x - 1 &&
                     squareList[i].getAttribute("y") == y - 1) {
-                    squareList[i].classList.add("moveOption");
+                    if (!turn) squareList[i].classList.add("moveOption");
                     squareList[i].onclick = (e) => {
                         if (!turn) {
-                            console.log(e.target);
                             parent.innerHTML = "";
                             addToken(e.target, type);
                             for (let j = 0; j < squareList.length; j++) {
